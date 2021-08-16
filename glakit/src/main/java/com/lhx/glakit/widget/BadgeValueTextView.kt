e90ceac42a4dd82128aedc2cc8df6d89864aa23e
罗海雄 <luohaixiong@zegobird.com>
2021-08-16 18:01:08 +0800

    1

--- glakit/src/main/java/com/lhx/glakit/widget/BadgeValueTextView.kt
+++ glakit/src/main/java/com/lhx/glakit/widget/BadgeValueTextView.kt
@@ -2,14 +2,17 @@ package com.lhx.glakit.widget
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import com.lhx.glakit.R
 import com.lhx.glakit.drawable.CornerBorderDrawable
+import com.lhx.glakit.extension.intValue
 import kotlin.math.max
 
 
@@ -23,48 +26,54 @@ class BadgeValueTextView : AppCompatTextView {
 
     //背景颜色
     @ColorInt
-    private var fillColor = Color.RED
-    set(value) {
-        if(value != field){
-            field = value
-            drawable.backgroundColor = field
+    var fillColor = Color.RED
+        set(value) {
+            if(value != field){
+                field = value
+                drawable.backgroundColor = field
+            }
         }
-    }
 
     //边框颜色
     @ColorInt
-    private var strokeColor = Color.TRANSPARENT
-    set(value) {
-        if(value != field){
-            field = value
-            drawable.borderColor = field
+    var strokeColor = Color.TRANSPARENT
+        set(value) {
+            if(value != field){
+                field = value
+                drawable.borderColor = field
+            }
         }
-    }
 
     //边框
-    private var strokeWidth = 0
-    set(value) {
-        if(value != field){
-            field = value
-            drawable.borderWidth = field
+    var strokeWidth = 0
+        set(value) {
+            if(value != field){
+                field = value
+                drawable.borderWidth = field
+            }
         }
-    }
+
+    //达到最大值的图标
+    var maxValueIconRes = 0
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
-
         if (attrs != null) {
             val array = getContext().obtainStyledAttributes(attrs, R.styleable.BadgeValueTextView)
 
             fillColor = array.getColor(R.styleable.BadgeValueTextView_badge_fill_color, Color.RED)
             strokeColor = array.getColor(R.styleable.BadgeValueTextView_badge_stroke_color, Color.TRANSPARENT)
             strokeWidth = array.getDimensionPixelOffset(R.styleable.BadgeValueTextView_badge_stroke_width, 0)
+            maxValueIconRes = array.getResourceId(R.styleable.BadgeValueTextView_badge_max_icon, 0)
 
             array.recycle()
         }
         setTextColor(Color.WHITE)
         gravity = Gravity.CENTER
+        if (text == null) {
+            text = ""
+        }
     }
 
     init {
@@ -90,25 +99,43 @@ class BadgeValueTextView : AppCompatTextView {
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+        if (text == null) {
+            text = ""
+        }
         hideIfNeeded()
     }
 
     override fun setText(text: CharSequence?, type: BufferType?) {
         var value = text
-        if (text != null && text.toString().toInt() > 99) {
-            value = "99+"
+        var drawable: Drawable? = null
+        if (text != null && text.toString().intValue() > 99) {
+            if (maxValueIconRes > 0) {
+                drawable = ContextCompat.getDrawable(context, maxValueIconRes)
+            } else {
+                value = "99+"
+            }
         }
-        super.setText(value, type)
+
+        if (drawable != null) {
+            value = ""
+            drawable.setBounds(0, 0, drawable.minimumWidth, drawable.minimumHeight)
+        }
+        setCompoundDrawables(drawable, null, null, null)
+
+        super.setText(value ?: "", type)
         hideIfNeeded()
     }
 
     //判断是否需要隐藏
     private fun hideIfNeeded() {
-        visibility = if (TextUtils.isDigitsOnly(text)) {
-            val value = text.toString().toInt()
-            if (value > 0) View.VISIBLE else View.INVISIBLE
-        } else {
-            if (TextUtils.isEmpty(text)) View.INVISIBLE else View.VISIBLE
+
+        visibility = when {
+            compoundDrawables[0] != null -> VISIBLE
+            TextUtils.isDigitsOnly(text) -> {
+                val value = text?.toString()?.intValue() ?: 0
+                if (value > 0) View.VISIBLE else View.GONE
+            }
+            else -> if (TextUtils.isEmpty(text)) View.GONE else View.VISIBLE
         }
     }
 }
