6080a3548be50ae32c01c6efb06028f842b4e7f8
罗海雄 <luohaixiong@zegobird.com>
2021-08-26 18:20:19 +0800

    1

--- app/src/main/java/com/lhx/glakitDemo/pager/PagerFragment.kt
+++ app/src/main/java/com/lhx/glakitDemo/pager/PagerFragment.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
+import com.lhx.glakit.adapter.RecyclerViewAdapter
 import com.lhx.glakit.base.fragment.BaseFragment
 import com.lhx.glakit.base.widget.BaseContainer
 import com.lhx.glakit.pager.CyclePageAdapter2
@@ -16,6 +20,7 @@ import com.lhx.glakitDemo.R
 class PagerFragment: BaseFragment() {
 
     val viewPager: ViewPager2 by lazy { findViewById(R.id.view_pager)!! }
+    val recyclerView: RecyclerView by lazy { findViewById(R.id.recyclerView)!! }
 
     override fun initialize(
         inflater: LayoutInflater,
@@ -27,6 +32,10 @@ class PagerFragment: BaseFragment() {
         adapter.shouldAutoPlay = true
         viewPager.adapter = adapter
         viewPager.setPageTransformer(MarginPageTransformer(pxFromDip(10f)))
+
+        PagerSnapHelper().attachToRecyclerView(recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        recyclerView.adapter = RAdapter(recyclerView)
     }
 
     private inner class Adapter(viewPager2: ViewPager2): CyclePageAdapter2(viewPager2) {
@@ -45,5 +54,26 @@ class PagerFragment: BaseFragment() {
         override val realCount: Int
             get() = colors.size
 
+    }
+
+    private inner class RAdapter(recyclerView: RecyclerView) : RecyclerViewAdapter(recyclerView) {
+
+        val colors = arrayOf(Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE, Color.GRAY)
+        override fun onCreateViewHolder(viewType: Int, parent: ViewGroup): RecyclerViewHolder {
+            return RecyclerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.pager_item_2, parent, false))
+        }
+
+        override fun numberOfItems(section: Int): Int {
+            return colors.size
+        }
+
+        override fun onBindItemViewHolder(
+            viewHolder: RecyclerViewHolder,
+            position: Int,
+            section: Int
+        ) {
+            viewHolder.getView<TextView>(R.id.text).text = "${position + 1}"
+            viewHolder.itemView.setBackgroundColor(colors[position])
+        }
     }
 }
